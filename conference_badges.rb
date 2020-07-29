ddacc524cc5900d527a5f408ff040c584fbf6daf
Keenah Vancampenhout <kvancamp@mail.com>
2020-07-29 00:02:47 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,3 +1,9 @@
 def badge_maker(name)
   return "Hello, my name is #{name}."
 end
+
+def batch_badge_creator(attendees)
+ attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+ 
+
+end
